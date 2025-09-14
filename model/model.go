3a4b51cf0cf6479e4f0051953b5f6fd23f5911ad
Babu Amos <amos.babu@yahoo.com>
2025-09-15 00:37:31 +0300

    "typo"

--- model/model.go
+++ model/model.go
@@ -3,11 +3,11 @@ package model
 import "time"
 
 type User struct {
-	Id        int64     `json: "id" db: "id"`
-	Name      string    `json: "name" db: "name"`
-	Email     string    `json: "email" db: "emaill"`
-	Password  string    `json: "-" db: "password"`
-	CreatedAt time.Time `json: "created_at" db: "created_at"`
+	Id        int64     `json:"id" db:"id"`
+	Name      string    `json:"name" db:"name"`
+	Email     string    `json:"email" db:"email"`
+	Password  string    `json:"-" db:"password"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
 }
 
 func NewUser(newUser User) *User {
